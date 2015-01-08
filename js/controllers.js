2ac70bcd0ce32baabcb1bec93ab1c5a03b6ac9b1
Bonnie <kbonnie@gmail.com>
2015-01-08 13:48:52 -0800

    pause to use old version

--- js/controllers.js
+++ js/controllers.js
@@ -1,23 +1,9 @@
 var nameSpace = angular.module("GuitarApp", []);
  
-nameSpace.controller("GuitarFunction", function GuitarFunction($scope)
+nameSpace.controller("GuitarFunction", function GuitarFunction($scope, $http)
 {    
-	$scope.guitarVariable = [
-{
-	"name" 	 : "Miss Piggy",
-	"description": "Miss Piggy is employed as a star actress under Muppets Inc.",
-	"image"	 :  "misspiggy"
-},
-{
-	"name" 	: "KErmit de Frog",
-	"description": "Kermit works in the swamp for Muppets Inc.",
-	"image"	:  "kermit"
-},
-{
-	"name" 	: "Big Bird",
-	"description": "Big Bird works for Muppets Enterprises in Accounts Payable",
-	"image"	:  "bigbird"
-}
-    ]
+	$http.get('js/data.json').success(function(data){
+		$scope.guitarVariable = data;
+	}) ;
 }
 );
