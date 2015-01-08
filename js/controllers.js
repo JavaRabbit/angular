814a8808f404c9d67e16a3455784a234d6fd8f79
Bonnie <kbonnie@gmail.com>
2015-01-08 14:49:04 -0800

    works end of using http

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,9 @@
 var nameSpace = angular.module("GuitarApp", []);
  
-nameSpace.controller("GuitarFunction", function GuitarFunction($scope, $http)
+nameSpace.controller("GuitarFunction", ['$scope', '$http', function($scope, $http)
 {    
 	$http.get('js/data.json').success(function(data){
 		$scope.guitarVariable = data;
 	}) ;
-}
+}]
 );
