cb497c839ad6ac3ba3a7ee6147f38c8a1195756a
Bonnie <kbonnie@gmail.com>
2015-01-08 11:14:44 -0800

    works, changes to contacts

--- js/controllers.js
+++ js/controllers.js
@@ -1,24 +1,23 @@
-var myApp = angular.module('myApp',[]);
-
-myApp.controller('MyController', function MyController($scope)
+var nameSpace = angular.module("GuitarApp", []);
+ 
+nameSpace.controller("GuitarFunction", function GuitarFunction($scope)
+{    
+	$scope.guitarVariable = [
 {
-		$scope.artists = [
-	{
-		"name":"Miss Piggy",
-		"species":"pig",
-		"description":"Miss Piggy is employed as a star actress under Muppets Inc."
-	},
-	{
-		"name":"KErmit de Frog",
-		"species":"Green non-poisonous frog",
-		"description":"Kermit works in the swamp for Muppets Inc."
-
-	},
-	{
-		"name":"KErmit de Frog",
-		"species":"Green non-poisonous frog",
-		"description":"Kermit works in the swamp for Muppets Inc."
-
-	}
-	]
-});
+	"name" 	 : "Miss Piggy",
+	"description": "Miss Piggy is employed as a star actress under Muppets Inc.",
+	"image"	 :  "Acoustic"
+},
+{
+	"name" 	: "KErmit de Frog",
+	"description": "Kermit works in the swamp for Muppets Inc.",
+	"image"	:  "Electric"
+},
+{
+	"name" 	: "Big Bird",
+	"description": "Big Bird works for Muppets Enterprises in Accounts Payable",
+	"image"	:  "Bass"
+}
+    ]
+}
+);
